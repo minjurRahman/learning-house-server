c745e00e581db2a81a4373922cf25becac17343a
Minjur <minjur.rahaman99@gmail.com>
2022-10-24 23:01:15 +0600

    checking server running or not

--- index.js
+++ index.js
@@ -3,4 +3,19 @@ const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 
-app.use(cors())
+app.use(cors());
+
+const categories = require('./data/categories.json');
+const news = require('./data/course.json');
+
+//Get response for running
+app.get('/', (req, res) =>{
+    res.send('Education plus API Running');
+});
+
+
+
+
+app.listen(port, () =>{
+    console.log('Education institute server running on port:', port )
+})
